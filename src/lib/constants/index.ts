731194cca044867fe4b942269d12819ddad49385
Kevin Jiang <kevin.jiang53@gmail.com>
2026-04-24 22:49:26 -0300

    Finalize constants barrel exports

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,12 +1,126 @@
-export * from "./paths";
-export * from "./storage";
-export * from "./jobs";
-export * from "./profile";
-export * from "./interview";
-export * from "./documents";
-export * from "./llm";
-export * from "./email";
-export * from "./reminders";
-export * from "./notifications";
-export * from "./resume-tracking";
-export * from "./backup";
+export { PATHS } from "./paths";
+
+export { THEMES, themeSchema, STORAGE_KEYS } from "./storage";
+export type { Theme } from "./storage";
+
+export {
+  JOB_STATUSES,
+  jobStatusSchema,
+  JOB_TYPES,
+  jobTypeSchema,
+  createJobSchema,
+  updateJobSchema,
+  importJobSchema,
+  importJobsArraySchema,
+  TECH_KEYWORDS,
+} from "./jobs";
+export type {
+  JobStatus,
+  JobType,
+  CreateJobInput,
+  UpdateJobInput,
+  ImportJobInput,
+} from "./jobs";
+
+export {
+  SKILL_CATEGORIES,
+  skillCategorySchema,
+  PROFICIENCY_LEVELS,
+  proficiencySchema,
+  contactInfoSchema,
+  experienceSchema,
+  educationSchema,
+  skillSchema,
+  projectSchema,
+  certificationSchema,
+  updateProfileSchema,
+} from "./profile";
+export type {
+  SkillCategory,
+  ProficiencyLevel,
+  UpdateProfileInput,
+} from "./profile";
+
+export {
+  INTERVIEW_CATEGORIES,
+  interviewCategorySchema,
+  INTERVIEW_DIFFICULTIES,
+  interviewDifficultySchema,
+  DIFFICULTY_DESCRIPTIONS,
+  startInterviewSchema,
+  interviewAnswerSchema,
+  SESSION_MODES,
+  sessionModeSchema,
+  SESSION_QUESTION_CATEGORIES,
+  sessionQuestionCategorySchema,
+  sessionQuestionSchema,
+  createInterviewSessionSchema,
+} from "./interview";
+export type {
+  InterviewCategory,
+  InterviewDifficulty,
+  InterviewAnswerInput,
+  SessionMode,
+  SessionQuestionCategory,
+  CreateInterviewSessionInput,
+} from "./interview";
+
+export {
+  DOCUMENT_TYPES,
+  documentTypeSchema,
+  MAX_FILE_SIZE_BYTES,
+  ALLOWED_MIME_TYPES,
+  FILE_SIGNATURES,
+  validateFileMagicBytes,
+  parseDocumentSchema,
+} from "./documents";
+export type { DocumentType, ParseDocumentInput } from "./documents";
+
+export {
+  LLM_PROVIDERS,
+  llmProviderSchema,
+  DEFAULT_LLM_TIMEOUT_MS,
+  llmConfigSchema,
+  updateSettingsSchema,
+  LLM_ENDPOINTS,
+  DEFAULT_MODELS,
+  DEFAULT_MODEL_BY_PROVIDER,
+} from "./llm";
+export type { LLMProvider, LLMConfigInput } from "./llm";
+
+export {
+  EMAIL_TEMPLATE_TYPES,
+  emailTemplateTypeSchema,
+  generateEmailSchema,
+} from "./email";
+export type { EmailTemplateType, GenerateEmailInput } from "./email";
+
+export {
+  REMINDER_TYPES,
+  reminderTypeSchema,
+  createReminderSchema,
+} from "./reminders";
+export type { ReminderType, CreateReminderInput } from "./reminders";
+
+export {
+  NOTIFICATION_ACTIONS,
+  notificationActionSchema,
+} from "./notifications";
+export type { NotificationAction } from "./notifications";
+
+export {
+  compareResumesSchema,
+  trackResumeSentSchema,
+  updateTrackingOutcomeSchema,
+} from "./resume-tracking";
+export type {
+  CompareResumesInput,
+  TrackResumeSentInput,
+  UpdateTrackingOutcomeInput,
+} from "./resume-tracking";
+
+export {
+  backupDataSchema,
+  fullExportDataSchema,
+} from "./backup";
+export type { BackupDataInput, FullExportDataInput } from "./backup";
