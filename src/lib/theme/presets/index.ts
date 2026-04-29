919d6da434485676a682f95b5a4ad2107395764b
Kevin Jiang <kevin.jiang53@gmail.com>
2026-04-29 16:56:15 -0300

    Clean up theme migration dead code

--- src/lib/theme/presets/index.ts
+++
@@ -1,7 +0,0 @@
-export { bloxyTheme } from "./bloxy";
-export { defaultTheme } from "./default";
-export { earthTheme } from "./earth";
-export { glassTheme } from "./glass";
-export { minimalTheme } from "./minimal";
-export { neonTheme } from "./neon";
-export { premiumTheme } from "./premium";
